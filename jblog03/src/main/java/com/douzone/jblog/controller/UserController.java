d05abf42aa3dad938b94d5be31520909a6013b49
eogkdfh <eogkdfh@gmail.com>
2021-11-01 01:23:13 +0900

    jblog03 #join

--- jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
@@ -1,18 +1,58 @@
 package com.douzone.jblog.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.douzone.jblog.service.UserService;
+import com.douzone.jblog.vo.UserVo;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join(@ModelAttribute UserVo vo) {
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			List<ObjectError> list = result.getAllErrors();
+			for(ObjectError error : list) {
+				System.out.println(error);
+			}
+			
+			Map<String, Object> map = result.getModel(); // userVo 이름의 객체로 담겨있다
+			model.addAttribute(map);
+			
+			return "user/join";
+		}
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping("/joinsuccess")
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
-		
-		return "";
+		return "user/login";
 	}
 	
 }
--- jblog03/src/main/java/com/douzone/jblog/service/UserService.java
+++ jblog03/src/main/java/com/douzone/jblog/service/UserService.java
@@ -1,19 +1,21 @@
 package com.douzone.jblog.service;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import com.douzone.jblog.repository.UserRepository;
+import com.douzone.jblog.vo.UserVo;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String login() {
-		return "user/login";
+
+	public boolean join(UserVo vo) {
+		return userRepository.insert(vo);
 	}
+	
 }
