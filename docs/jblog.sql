5d56ed41213186e178140e1ccba50d9e92d4f1fb
eogkdfh <eogkdfh@gmail.com>
2021-11-09 23:14:13 +0900

    sql

--- docs/jblog.sql
+++ docs/jblog.sql
@@ -4,6 +4,13 @@ insert
 into user 
 values('bolt', '창현', 1234, now());
 
+select count(*)
+from post
+where category_no = 10;
+
+select c.no, c.name, c.desc, c.blog_id as blogId, (select count(*) from post where category_no = c.no) as postCount
+from category c;
+
 select * from user;
 delete from user where id = 'bolt';
 
@@ -11,7 +18,8 @@ select * from blog;
 
 
 desc category;
-select c.no, c.name, c.desc, c.blog_id as blogId from category c;
+select c.no, c.name, c.desc, c.blog_id as blogId 
+from category c;
 
 insert into category values(null, 'test', 'java', 'nami');
 delete from category where no = 4;
